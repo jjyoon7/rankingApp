3ff30b2a56c3ab02339b1aa7dc9bf967428472b0
jjyoon7 <jay.yoon7@gmail.com>
2020-10-08 21:07:40 +0200

    delete highestScorePerUser function, sinc it is not needed

--- src/Context.js
+++ src/Context.js
@@ -12,48 +12,32 @@ function ContextProvider(props) {
     const hasUsers = users.length > 0
     const hasScores = scores.length > 0
 
-
-    const highestScorePerUser = array => array.reduce((accumulator, current) => {
-        if (checkIfIdExists(current)) {  
-        //   console.log('accumulator',accumulator)
-          return accumulator
-        } else {
-          return [...accumulator, current]
-        }
-      
-        function checkIfIdExists(currentVal) {
-            return accumulator.some((item) => {
-                // console.log('item',item)
-                // console.log('currentVal',currentVal)
-              if(item.userId === currentVal.userId) {
-                   const result = Math.max(currentVal.score, item.score)
-                   item.score = result
-                   return item.userId === currentVal.userId
-              }
-            })
-        }
-    }, [])
-
     //sort array
     const sortArrDescending = array => array.sort((prevValue, currentValue) => {
         // console.log('prev', prevValue)
         // console.log('currentValue', currentValue)
+
+        //if the array which contains user objecst,
+        //needs to be sorted, based on user's highest score
+        if(prevValue.scoreArray !== undefined) {
+            return currentValue.scoreArray[0] - prevValue.scoreArray[0]
+        }
         return currentValue - prevValue
     })
 
-    //merge highestScorePerUser and add those fields to user obj.
-    const addScoreToUser = (scoreArray, userArray) => {
-        const userWithScoreArray = userArray.map(user => {
+    //add score to the user
+    const addScoreToUser = (scoreArray, usersArray) => {
+        const userWithScoreArray = usersArray.map(user => {
             user.scoreArray = []
             for (var i = 0; i < scoreArray.length; i++) {
                 if(scoreArray[i].userId === user._id) {
                     user.scoreArray.push(scoreArray[i].score)
                 }
             }
+            //sort the score array of user in descending order
             const array = user.scoreArray
             sortArrDescending(array)
             return user
-            // sortArrDescending(user.scoreArray)
         })
         return userWithScoreArray
     }
@@ -61,18 +45,19 @@ function ContextProvider(props) {
     //when app is first initially loaded
     //save users and scores in state
     useEffect(() => {
-        // console.log('hasUsers && hasScores', hasUsers && hasScores)
         if(hasUsers && hasScores) {
-
-            // const highestScores = highestScorePerUser(scores)
             const userWithScores = addScoreToUser(scores, users)
-            console.log('userWithScores',userWithScores)
-            // const sortedUsers = sor
-            setUsersArr(userWithScores)
+            const sortedUsers = sortArrDescending(userWithScores)
+            setUsersArr(sortedUsers)
         } else {
             //show error
         }
     }, [])
+
+    //to check if usersArr is updated with correct order
+    useEffect(() => {
+        console.log('usersArr', usersArr)
+    }, [ usersArr ])
 
 
     return (
