ba181fc44dd729e8955be266e43bf1e7b1cf81ea
jjyoon7 <jay.yoon7@gmail.com>
2020-10-09 11:41:49 +0200

    update logic for UserInputForm component

--- src/components/UserInputForm/UserInputForm.js
+++ src/components/UserInputForm/UserInputForm.js
@@ -3,32 +3,45 @@ import { Context } from '../../Context'
 import users from '../../users'
 
 export default function UserInputForm() {
-    const { usersArr, setUsersArr } = useContext(Context)
+    const { usersArr, setUsersArr, sortArrDescending } = useContext(Context)
     const [ userName, setUserName ] = useState('')
     const [ userScore, setUserScore ] = useState(0)
     const [ userObj, setUserObj ] = useState({})
+
+    const userAlreadyExists = usersArr.some(user => {
+        return user.name === userName
+    })
 
     //enter a name and a score. If the user name does not already exist, that name should be added to the list of users. If the user does exist, 
     //the score should be added to them and the ranking list updated if needed.
     const onSubmitUserInputForm = (e) => {
         e.preventDefault()
+        console.log('userAlreadyExists',userAlreadyExists)
+        if(userAlreadyExists) {
+            //but this will render as usersArr.length
+            //thus else statement where creating new userObj will run
+            const updatedUsersArr = usersArr.map(user => {
+                    if(user.name === userName) {
+                        user.scoreArray.push(userScore)
+                        //order the list
+                        return user
+                        // sortArrDescending(updatedUserArr)
+                    } else return user
+            })
 
-        const updatedUsersArr = usersArr.map(user => {
-            if(user.name === userName) {
-                console.log('user.scoreArray',user.scoreArray)
-                return user.scoreArray.push(userScore)
-            } else {
-                console.log('userScore',userScore)
+            setUsersArr(updatedUsersArr)
+
+            console.log('updatedUsersArr',updatedUsersArr)
+        } else if(!userAlreadyExists) {
+                // console.log('userScore', userScore)
                 const newUserObj = {
-                    _id: '',
+                    _id: usersArr.length + 1,
                     name: userName,
-                    scoreArray: [...userScore]
+                    scoreArray: [userScore]
                 }
                 return usersArr.push(newUserObj)
-            }
-        })
-        setUsersArr(updatedUsersArr)
-        console.log('updatedUsersArr',updatedUsersArr)
+            
+        }
     }
 
     const onChangeName = (e) => setUserName(e.target.value)
@@ -38,10 +51,10 @@ export default function UserInputForm() {
         setUserScore(parsedIntValue)
     }
 
-    useEffect(() => {
-        console.log('userName', userName)
-        console.log('userScore', userScore)
-    }, [ userName, userScore ])
+    // useEffect(() => {
+    //     console.log('userName', userName)
+    //     console.log('userScore', userScore)
+    // }, [ userName, userScore ])
 
     return (
         <div className=''>
@@ -50,7 +63,7 @@ export default function UserInputForm() {
                     <input id='name' type='text' value={userName} onChange={onChangeName}/>
                 </label>
                 <label htmlFor='score'>
-                    <input id='score' type='text' value={userScore} onChange={onChangeScore}/>
+                    <input id='score' type='text' onChange={onChangeScore}/>
                 </label>
                 <input type="submit" value="Add"/>
             </form>
