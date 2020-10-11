847a21320c2f9ee5c957211b5bc53c3e167efdff
jjyoon7 <jay.yoon7@gmail.com>
2020-10-11 20:29:21 +0200

    clean up

--- src/components/IndividualUserScoreList/IndividualUserScoreList.js
+++ src/components/IndividualUserScoreList/IndividualUserScoreList.js
@@ -1,32 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Context } from '../../Context'
 
-// import useModal from '../../customHooks/useModal/useModal'
-
 export default function IndividualUserScoreList() {
     const { usersArr, userScoreArr } = useContext(Context)
     const [ IndividualUserScoreList, setIndividualUserScoreList ] = useState(null)
 
-    // const IndividualUserScoreListModal = useModal(isModalOpen, IndividualUserScoreList)
-
-    const onClickIndividualUserScoreList = () => {
-
-    }
-
     useEffect(() => {
         const mappedScoreList = userScoreArr.map(score => {
-            // console.log('score', score)
             return <li key={score} className='score-li'>
                         {score}
                   </li>
         })
         
         setIndividualUserScoreList(mappedScoreList)
-        // console.log('mappedScoreList', mappedScoreList)
     }, [ userScoreArr, usersArr ])
 
     return (
-        <div className='user-score-list-div' onClick={onClickIndividualUserScoreList}>
+        <div className='user-score-list-div'>
             <ul className='user-score-list-ul'>
                 {IndividualUserScoreList}
             </ul>
