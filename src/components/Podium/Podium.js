7830a9f87882c4028c6076f6fcdcb1a9bac12bfd
jjyoon7 <jay.yoon7@gmail.com>
2020-10-15 16:52:52 +0200

    add medal icon, and clean up

--- src/components/Podium/Podium.js
+++ src/components/Podium/Podium.js
@@ -10,28 +10,27 @@ export default function Podium() {
     const [ thirdPlace, setThirdPlace ] = useState('')
     const hasUsers = usersArr.length > 0
     useEffect(() => {
-
         if(hasUsers) {
             setFirstPlace(usersArr[0].name)
             setSecondPlace(usersArr[1].name)
             setThirdPlace(usersArr[2].name)
-            // console.log('mappedFirstPlace', mappedFirstPlace)
         }
     }, [ usersArr ])
 
     return  (
         <div className="podium">
-            <h2 className="podium-left podium-name second-place-name-color">{secondPlace}</h2>
+            <h2 className="podium-left podium-name">{secondPlace}</h2>
             <div className="podium-div-style podium-left">
               <h1 className='number-typeface'>2</h1>
             </div>
 
-            <h2 className="podium-center podium-name first-place-name-color">{firstPlace}</h2>
+            <div className="podium-center podium-name podium-medal"><i className="ri-medal-line podium-medal"></i></div>
+            <h2 className="podium-center podium-name">{firstPlace}</h2>
             <div className="podium-div-style podium-center">
               <h1 className='number-typeface'>1</h1>
             </div>
 
-            <h2 className="podium-right podium-name third-place-name-color">{thirdPlace}</h2>
+            <h2 className="podium-right podium-name">{thirdPlace}</h2>
             <div className="podium-div-style podium-right">
               <h1 className='number-typeface'>3</h1>
             </div>
